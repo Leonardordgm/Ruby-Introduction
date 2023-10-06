912ea3e66cd595cc5cb7a64d3657a9c52071f344
Leonardo Rodrigues Martins <117056433+Leonardordgm@users.noreply.github.com>
2023-10-05 21:20:45 -0300

    Create Input-form.rb

---
+++ Input-form.rb
@@ -0,0 +1,10 @@
+puts "Seu nome: "
+name = gets.chomp
+
+puts "Sua idade: "
+idade = gets.chomp
+
+puts "Sua cidade: "
+cidade = gets.chomp
+
+puts "Olá, #{name}, você tem #{idade} e mora em #{cidade}!"
