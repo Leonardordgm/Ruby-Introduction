676a3c53c675c925eab4686a381f054257432beb
Leonardo Rodrigues Martins <117056433+Leonardordgm@users.noreply.github.com>
2023-10-08 15:06:14 -0300

    Create calculadora.rb

---
+++ calculadora.rb
@@ -0,0 +1,7 @@
+puts "Digite qualquer número: "
+num1 = gets.chomp.to_i #.to_i é para o valor sempre retornar um número
+
+puts "Digite outro Número: "
+num2 = gets.chomp.to_i
+
+puts "o valor da soma de #{num1} + #{num2} é #{num1 + num2}"
