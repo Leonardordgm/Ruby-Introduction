f8948cecadadad94192028a8a05a4245416a60ba
Leonardo Rodrigues Martins <117056433+Leonardordgm@users.noreply.github.com>
2023-10-14 02:29:35 -0300

    Create times.rb

---
+++ times.rb
@@ -0,0 +1,10 @@
+20.times do |contador|
+    puts contador
+  end
+  
+  count = 1
+  for contador in 1..20 #da para passar sem ser uma lista e com for
+    puts count
+    count = count + 1
+  end
+  
